395ae66175f92cd6fad9f3cc8f2eb96a621d59b3
Uros Marinkovic <38292385+uros2929@users.noreply.github.com>
2019-03-07 16:35:31 +0100

    first commit for Tablic

--- src/app/tablic/tablic.component.ts
+++ src/app/tablic/tablic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeckService } from '../deck.service';
 
 @Component({
   selector: 'app-tablic',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablicComponent implements OnInit {
 
-  constructor() { }
+  deckArr;
+  player1Arr = [];
+  tableArr = [];
+  player2Arr = [];
+
+  constructor(private _deckService: DeckService) { }
 
   ngOnInit() {
+    this._deckService.createCardObj();
+    this._deckService.shuffleDeck();
+    this.deckArr = this._deckService.deck;
+    this.firstDeal();
+  }
+
+  firstDeal(){
+    this.dealCardsForTable();
+    this.dealCardsToPlayer1();
+    this.dealCardsToPlayer2();
+    this.dealCardsToPlayer1();
+    this.dealCardsToPlayer2();
+  }
+
+  dealCardsToPlayer1() {
+    for (let index = 0; index < 3; index++) {
+      let newCard = this.deckArr.shift();
+      this.player1Arr.push(newCard)
+    }
+  }
+  dealCardsForTable() {
+    for (let index = 0; index < 4; index++) {
+      let newCard = this.deckArr.shift();
+      this.tableArr.push(newCard)
+    }
+  }
+  dealCardsToPlayer2() {
+    for (let index = 0; index < 3; index++) {
+      let newCard = this.deckArr.shift();
+      this.player2Arr.push(newCard)
+    }
+  }
+  cardClicked(event){
+    if (event.target.style.border=="") {
+      event.target.style.border="3px solid red"
+    }else{
+      event.target.style.border=""
+    }
+    console.log(event.target.className)
   }
 
 }
